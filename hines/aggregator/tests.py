26e3fe617a5ccb257ae4308c89635e7adca98a63
Phil Gyford <phil@gyford.com>
2010-12-29 11:54:28 +0000

    Change fixtures path.

--- hines/aggregator/tests.py
+++ hines/aggregator/tests.py
@@ -5,7 +5,7 @@ from django.conf import settings
 
 
 class AggregatorBaseTestCase(TestCase):
-    fixtures = ['test_data.json', ]
+    fixtures = ['../fixtures/test_data.json', ]
 
 
 class ViewsTestCase(AggregatorBaseTestCase):
