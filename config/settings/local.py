fb68c67d1921ef656dd5f5bb2338b9218d9abffe
Phil Gyford <phil@gyford.com>
2017-03-29 16:42:54 +0100

    Add debug toolbar

--- config/settings/local.py
+++ config/settings/local.py
@@ -4,3 +4,17 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*",]
 
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+
+# Debug Toolbar settings.
+if DEBUG:
+    MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware', ]
+    INSTALLED_APPS += ['debug_toolbar', ]
+    INTERNAL_IPS = ['127.0.0.1', '192.168.33.1', '0.0.0.0', '109.246.8.73',]
+
