a9423431aca02ec3e5b3fc5c5310ebf4d40eac25
Phil Gyford <phil@gyford.com>
2017-03-29 16:33:06 +0100

    Add core app

--- config/settings/defaults.py
+++ config/settings/defaults.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'CHANGEME')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False 
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'hines.core',
 ]
 
 MIDDLEWARE = [
---
+++ hines/core/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CoreConfig(AppConfig):
+    name = 'hines.core'
+    verbose_name = 'Core'
