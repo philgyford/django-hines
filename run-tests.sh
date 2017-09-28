785988475d4ed9fa79b18d4a7dc738ce24cad151
Phil Gyford <phil@gyford.com>
2017-09-28 15:57:00 +0100

    Allow an optional test name to be passed in to run-tests.sh

--- run-tests.sh
+++ run-tests.sh
@@ -1,5 +1,10 @@
 #! /bin/bash
 
-coverage run --branch --source=. --omit=*/migrations/*.py,manage.py,tests/*.py manage.py test --settings=config.settings.tests
+# You can optionally pass in a test, or test module or class, as an argument.
+# e.g.
+# ./run_tests.sh tests.appname.test_models.TestClass.test_a_thing
+TESTS_TO_RUN=${1:tests}
+
+coverage run --branch --source=. --omit=*/migrations/*.py,manage.py,tests/*.py manage.py test --settings=config.settings.tests $TESTS_TO_RUN
 #coverage report
 coverage html
