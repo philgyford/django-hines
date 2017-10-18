e927918bb0d1153798972a15a2ab758b0b0969b3
Phil Gyford <phil@gyford.com>
2017-10-18 19:01:00 +0100

    Get rid of unused development static file url

--- config/urls.py
+++ config/urls.py
@@ -90,6 +90,7 @@ if settings.DEBUG:
     urlpatterns += \
         static.static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-    urlpatterns += \
-        static.static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    # Don't think we need this now we use Whitenoise in development too:
+    # urlpatterns += \
+        # static.static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
