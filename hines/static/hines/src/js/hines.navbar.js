2efc757284e45b906a17f995c4812872246a1729
Phil Gyford <phil@gyford.com>
2017-11-21 18:21:19 +0000

    Possibly make the narrow-width search bar more accessible
    For #108

--- hines/static/hines/src/js/hines.navbar.js
+++ hines/static/hines/src/js/hines.navbar.js
@@ -2,7 +2,7 @@
  * Handles showing the navbar search form.
  *
  * 1. Adds a 'js-can-navbar' class to the body (useful for CSS).
- * 2. Adds a new <li><a></a></li> element to the navbar's <ul>.
+ * 2. Adds a new <li><button></button></li> element to the navbar's <ul>.
  * 3. Listens for a click on that. If it happens...
  * 4. ...hides that <li> and shows the search form.
  *
@@ -16,8 +16,6 @@
  *  navbar.init();
  *
  * Relies on our utils.js.
- *
- *
  */
 ;(function(){
   "use strict";
@@ -32,11 +30,12 @@
     // The nav <li> that we'll make and hide:
     var liClass = 'js-navbar-item-search';
 
-    // The mav <a> that we'll make, which will reveal the search form:
-    var aClass = 'js-navbar-nav-item-search';
+    // The mav <button> that we'll make, which will reveal the search form:
+    var buttonClass = 'js-navbar-nav-item-search';
 
     // The search form:
     var formClass = 'js-navbar-form';
+    var formId = 'js-navbar-form';
 
     var exports = {
 
@@ -55,15 +54,16 @@
      * the nav <ul>.
      */
     function createSearchLink() {
-      var a = document.createElement('a');
-      a.setAttribute('href', '#');
-      a.setAttribute('class', 'nav__link ' + aClass);
-      a.textContent = 'Search';
+      var button = document.createElement('button');
+      button.setAttribute('aria-expanded', 'false');
+      button.setAttribute('aria-controls', formId);
+      button.setAttribute('class', 'button--link nav__link ' + buttonClass);
+      button.textContent = 'Search';
 
       var li = document.createElement('li');
       li.setAttribute('class', 'nav__item nav__item--split ' + liClass);
 
-      li.appendChild(a);
+      li.appendChild(button);
 
       var nav = getElByClass(navClass);
 
@@ -71,10 +71,14 @@
     };
 
     function initListener() {
-      getElByClass(aClass).addEventListener('click', function(event) {
+      getElByClass(buttonClass).addEventListener('click', function(event) {
         event.preventDefault();
+
         hideEl(liClass);
         showEl(formClass, 'block');
+
+        var button = getElByClass(buttonClass);
+        button.setAttribute('aria-expanded', 'true');
       });
     };
     return exports;
