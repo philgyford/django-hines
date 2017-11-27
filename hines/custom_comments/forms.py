7b05f6dc0f47245c2af94b04e322a9377681a4d1
Phil Gyford <phil@gyford.com>
2017-11-27 16:42:24 +0000

    Styling comment form and adding field attributes
    For #108

--- hines/custom_comments/forms.py
+++ hines/custom_comments/forms.py
@@ -9,3 +9,17 @@ class CustomCommentForm(CommentForm):
         # Use our custom comment model instead of the built-in one.
         return CustomComment
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['name'].required = True
+        self.fields['name'].widget.attrs['required'] = 'required'
+
+        self.fields['email'].required = True
+        self.fields['email'].widget.attrs['required'] = 'required'
+        self.fields['email'].help_text = 'Will not be displayed.'
+
+        self.fields['url'].help_text = 'e.g. Your personal website, Twitter URL, etc.'
+
+        self.fields['comment'].required = True
+        self.fields['comment'].widget.attrs['required'] = 'required'
+
