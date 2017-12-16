f91fdb73f01ac78a865203966998e5bc4025f4b7
Phil Gyford <phil@gyford.com>
2017-12-16 10:51:30 +0000

    Set fake secret key for travis tests

--- config/settings/tests.py
+++ config/settings/tests.py
@@ -1,7 +1,5 @@
 from .base import *
 
-SECRET_KEY = 'MY-SECRET-KEY'
-
 ALLOWED_HOSTS = ["*",]
 
 MEDIA_ROOT = os.path.join(APPS_DIR, 'media')
@@ -9,3 +7,4 @@ MEDIA_URL = '/media/'
 
 
 HINES_ROOT_DIR = 'terry'
+
