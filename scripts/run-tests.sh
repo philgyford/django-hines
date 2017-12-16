8fb18c0d48ed4756cabd70e85ee35832df93bee4
Phil Gyford <phil@gyford.com>
2017-12-16 12:10:33 +0000

    Try to get coveralls working

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -6,5 +6,6 @@ set -e
 # ./run_tests.sh tests.appname.test_models.TestClass.test_a_thing
 TESTS_TO_RUN=${1:tests}
 
-./manage.py test --settings=config.settings.tests $TESTS_TO_RUN
-
+coverage run --branch --source=. --omit=*/migrations/*.py,manage.py,tests/*.py manage.py test --settings=config.settings.tests $TESTS_TO_RUN
+coverage report
+coverage html
