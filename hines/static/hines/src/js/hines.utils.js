dc35809c9c23d609f8cef7c19a2eee48e8da042f
Phil Gyford <phil@gyford.com>
2018-01-01 13:59:52 +0000

    Move JS ready function into utils

--- hines/static/hines/src/js/hines.utils.js
+++ hines/static/hines/src/js/hines.utils.js
@@ -45,3 +45,42 @@ $.addClass = function(el, className) {
     el.className += ' ' + className;
   };
 };
+
+
+/**
+ * domready (c) Dustin Diaz 2014 - License MIT
+ * https://github.com/ded/domready
+ *
+ * Use like:
+ *
+ * domready(function(){
+ *  console.log("We're ready!");
+ * });
+ */
+!function (name, definition) {
+
+  if (typeof module != 'undefined') module.exports = definition()
+  else if (typeof define == 'function' && typeof define.amd == 'object') define(definition)
+  else this[name] = definition()
+
+}('domready', function () {
+
+  var fns = [], listener
+    , doc = typeof document === 'object' && document
+    , hack = doc && doc.documentElement.doScroll
+    , domContentLoaded = 'DOMContentLoaded'
+    , loaded = doc && (hack ? /^loaded|^c/ : /^loaded|^i|^c/).test(doc.readyState)
+
+
+  if (!loaded && doc)
+  doc.addEventListener(domContentLoaded, listener = function () {
+    doc.removeEventListener(domContentLoaded, listener)
+    loaded = 1
+    while (listener = fns.shift()) listener()
+  })
+
+  return function (fn) {
+    loaded ? setTimeout(fn, 0) : fns.push(fn)
+  }
+
+});
