15eb4b0d6eaf36db54f6b39fa5fa82914570d452
Phil Gyford <phil@gyford.com>
2018-01-04 18:29:28 +0000

    Try make ssh tunnel happen in background

--- .profile.d/ssh_setup.sh
+++ .profile.d/ssh_setup.sh
@@ -2,7 +2,7 @@
 
 # Creates an SSH tunnel to the remote host where our old MySQL database runs.
 
-SSH_CMD="ssh -L 3307:127.0.0.1:3306 ${REMOTE_HOST_USER}@${REMOTE_HOST_NAME}"
+SSH_CMD="ssh -f -L 3307:127.0.0.1:3306 ${REMOTE_HOST_USER}@${REMOTE_HOST_NAME}"
 
 PID=`pgrep -f "${SSH_CMD}"`
 if [ $PID ] ; then
