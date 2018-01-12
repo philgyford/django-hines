0a9839d934392bee8ea162c9debc1de18020eee8
Phil Gyford <phil@gyford.com>
2018-01-12 17:38:25 +0000

    Update/fix Memcachier settings

--- config/settings/production.py
+++ config/settings/production.py
@@ -28,40 +28,46 @@ MEDIA_URL = 'https://{}.s3.amazonaws.com{}'.format(
 
 
 # See https://devcenter.heroku.com/articles/memcachier#django
-environ['MEMCACHE_SERVERS'] = get_env_variable('MEMCACHIER_SERVERS').replace(',', ';')
-environ['MEMCACHE_USERNAME'] = get_env_variable('MEMCACHIER_USERNAME')
-environ['MEMCACHE_PASSWORD'] = get_env_variable('MEMCACHIER_PASSWORD')
+memcache_username = get_env_variable('MEMCACHIER_USERNAME')
+memcache_password = get_env_variable('MEMCACHIER_PASSWORD')
+memcache_servers = get_env_variable('MEMCACHIER_SERVERS')
 
 CACHES = {
     'default': {
         'BACKEND': 'django_pylibmc.memcached.PyLibMCCache',
-
-        # Use binary memcache protocol (needed for authentication)
-        'BINARY': True,
 
         # TIMEOUT is not the connection timeout! It's the default expiration
         # timeout that should be applied to keys! Setting it to `None`
         # disables expiration.
         'TIMEOUT': None,
 
+        'LOCATION': memcache_servers,
+
         'OPTIONS': {
-            # Enable faster IO
-            'tcp_nodelay': True,
+             # Use binary memcache protocol (needed for authentication)
+            'binary': True,
+            'username': memcache_username,
+            'password': memcache_password,
+            'behaviors': {
+                # Enable faster IO
+                'no_block': True,
+                'tcp_nodelay': True,
 
-            # Keep connection alive
-            'tcp_keepalive': True,
+                # Keep connection alive
+                'tcp_keepalive': True,
 
-            # Timeout settings
-            'connect_timeout': 2000, # ms
-            'send_timeout': 750 * 1000, # us
-            'receive_timeout': 750 * 1000, # us
-            '_poll_timeout': 2000, # ms
+                # Timeout settings
+                'connect_timeout': 2000, # ms
+                'send_timeout': 750 * 1000, # us
+                'receive_timeout': 750 * 1000, # us
+                '_poll_timeout': 2000, # ms
 
-            # Better failover
-            'ketama': True,
-            'remove_failed': 1,
-            'retry_timeout': 2,
-            'dead_timeout': 30,
+                # Better failover
+                'ketama': True,
+                'remove_failed': 1,
+                'retry_timeout': 2,
+                'dead_timeout': 30,
+            }
         }
     }
 }
