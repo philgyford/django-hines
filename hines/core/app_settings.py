e8f500a6fd97ddb54bd6188fd6d9b26128ccde55
Phil Gyford <phil@gyford.com>
2018-04-17 16:50:27 +0100

    Order settings

--- hines/core/app_settings.py
+++ hines/core/app_settings.py
@@ -3,7 +3,6 @@ from django.conf import settings
 
 ALLOW_COMMENTS = getattr(settings, 'HINES_ALLOW_COMMENTS', True)
 
-# Default TODO. Bleach.
 COMMENTS_ALLOWED_TAGS = getattr(settings, 'HINES_COMMENTS_ALLOWED_TAGS', [
     'a',
     'abbr',
@@ -19,7 +18,6 @@ COMMENTS_ALLOWED_TAGS = getattr(settings, 'HINES_COMMENTS_ALLOWED_TAGS', [
     'ul',
 ])
 
-# Default TODO. Bleach.
 COMMENTS_ALLOWED_ATTRIBUTES = getattr(settings, 'HINES_COMMENTS_ALLOWED_ATTRIBUTES', {
     'a': ['href', 'title'],
     'acronym': ['title'],
@@ -30,10 +28,10 @@ FIRST_DATE = getattr(settings, 'HINES_FIRST_DATE', False)
 
 GOOGLE_ANALYTICS_ID = getattr(settings, 'HINES_GOOGLE_ANALYTICS_ID', '')
 
+HOME_PAGE_DISPLAY = getattr(settings, 'HINES_HOME_PAGE_DISPLAY', {})
+
 ROOT_DIR = getattr(settings, 'HINES_ROOT_DIR', '')
 
 TEMPLATE_SETS = getattr(settings, 'HINES_TEMPLATE_SETS', None)
 
 USE_HTTPS = getattr(settings, 'HINES_USE_HTTPS', False)
-
-HOME_PAGE_DISPLAY = getattr(settings, 'HINES_HOME_PAGE_DISPLAY', {})
