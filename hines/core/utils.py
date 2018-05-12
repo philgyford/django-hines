e4a8a9ede211e8916709a65de4df0934322cf14e
Phil Gyford <phil@gyford.com>
2018-05-12 18:03:33 +0100

    Add method for invalidating cached views
    For #171

--- hines/core/utils.py
+++ hines/core/utils.py
@@ -45,3 +45,39 @@ def truncate_string(text, strip_html=True, chars=255, truncate=u'…', at_word_b
         text = Truncator(text).chars(chars, html=False, truncate=truncate)
     return text
 
+
+def expire_view_cache(view_name, args=[], namespace=None, key_prefix=None, method="GET"):
+    """
+    This function allows you to invalidate any view-level cache.
+        view_name: view function you wish to invalidate or it's named url pattern
+        args: any arguments passed to the view function
+        namepace: optioal, if an application namespace is needed
+        key prefix: for the @cache_page decorator for the function (if any)
+        from: https://gist.github.com/1223933
+        which said:
+        from: http://stackoverflow.com/questions/2268417/expire-a-view-cache-in-django
+        added: method to request to get the key generating properly
+
+    (Also used on pepysdiary.)
+    """
+    from django.urls import reverse
+    from django.http import HttpRequest
+    from django.utils.cache import get_cache_key
+    from django.core.cache import cache
+    from django.conf import settings
+    # create a fake request object
+    request = HttpRequest()
+    request.method = method
+    if settings.USE_I18N:
+        request.LANGUAGE_CODE = settings.LANGUAGE_CODE
+    # Loookup the request path:
+    if namespace:
+        view_name = namespace + ":" + view_name
+    request.path = reverse(view_name, args=args)
+    # get cache key, expire if the cached item exists:
+    key = get_cache_key(request, key_prefix=key_prefix)
+    if key:
+        if cache.get(key):
+            cache.set(key, None, 0)
+        return True
+    return False
