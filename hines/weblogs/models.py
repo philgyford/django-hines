b2bbc99633f96c071ac3e7306b44bb6a16c283d7
Phil Gyford <phil@gyford.com>
2018-05-15 15:55:11 +0100

    Only try to expire a Post's detail view cache if it has a time_published

--- hines/weblogs/models.py
+++ hines/weblogs/models.py
@@ -202,9 +202,12 @@ class Post(TimeStampedModelMixin, models.Model):
         super().save(*args, **kwargs)
 
         # Expire old detail page, home page, and blog home page.
-        expire_view_cache(self.get_absolute_url())
+        # Assumes the things used to generate the absolute_url haven't changed.
         expire_view_cache(reverse('home'))
-        expire_view_cache(reverse('weblogs:blog_detail', kwargs={'blog_slug': self.blog.slug}))
+        expire_view_cache(reverse('weblogs:blog_detail',
+                                        kwargs={'blog_slug': self.blog.slug}))
+        if self.time_published is not None:
+            expire_view_cache(self.get_absolute_url())
 
     def get_absolute_url(self):
         return reverse('weblogs:post_detail',
