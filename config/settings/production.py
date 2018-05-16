1a9d951325f23dd6f33d000ed36e070281ee28d9
Phil Gyford <phil@gyford.com>
2018-05-16 16:13:48 +0100

    Add SSL settings

--- config/settings/production.py
+++ config/settings/production.py
@@ -74,11 +74,14 @@ CACHES = {
 
 
 # https
-# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-# SECURE_SSL_REDIRECT = True
-# SESSION_COOKIE_SECURE = True
-# CSRF_COOKIE_SECURE = True
-# # HSTS
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
+# HSTS
+# Applies to the ENTIRE domain... so maybe not?
+# e.g. guardian.gyford.com, archive.gyford.com?
 # SECURE_HSTS_SECONDS = 31536000 # 1 year
 # SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 # SECURE_HSTS_PRELOAD = True
