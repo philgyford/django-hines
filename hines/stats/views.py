afa32f1183e66b73f7d6a385170388b81249150b
Phil Gyford <phil@gyford.com>
2018-10-23 13:06:29 +0100

    Reorganise how we get data for individual charts

--- hines/stats/views.py
+++ hines/stats/views.py
@@ -7,24 +7,73 @@ class HomeView(TemplateView):
 
 
 class StatsView(TemplateView):
-    template_name = 'stats/home.html'
+    template_name = 'stats/stats.html'
 
-    valid_slugs = [
-                    'music',
-                    ]
+    pages = [
+        {
+            'slug': 'music',
+            'title': 'Music',
+            'charts': [
+                'reading_books_per_year',
+            ]
+        },
+    ]
 
-    def get_template_names(self):
-        slug = self.kwargs.get('slug', None)
+    def get(self, request, *args, **kwargs):
+        "Ensure the slug is valid for our pages."
+        slug = kwargs.get('slug', None)
 
-        if slug is None:
-            return [self.template_name]
-        elif slug in self.valid_slugs:
-            return ['stats/{}.html'.format(slug)]
+        valid_slugs = [p['slug'] for p in self.pages]
+
+        if slug in valid_slugs:
+            return super().get(request, *args, **kwargs)
         else:
             raise Http404(("'{}' is not a valid slug.").format(slug))
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['pages'] = self.valid_slugs
-        context['current_page'] = self.kwargs.get('slug', None)
+
+        context['pages'] = self.pages
+
+        # Include the data for the current page in addition, separately:
+        for page in self.pages:
+            if page['slug'] == self.kwargs.get('slug'):
+                context['current_page'] = page
+                break
+
+        # Get the data for all of the charts on this page.
+        context['charts'] = []
+        for chart in context['current_page']['charts']:
+            context['charts'].append( self.get_chart_data(chart) )
+
         return context
+
+    def get_chart_data(self, chart_name):
+        """
+        Passed a chart_name (like 'reading_per_year') it returns a dict of
+        data for its chart, including:
+            'title'
+            'description'
+            'data' - A list of data for the chart
+        """
+        chart_data = {'name': chart_name,}
+
+        if chart_name == 'reading_books_per_year':
+            chart_data['title'] = 'Reading books per year'
+            chart_data['description'] = 'My description'
+            chart_data['data'] = [
+                {
+                  'label': '2001',
+                  'value': 30
+                },
+                {
+                  'label': '2002',
+                  'value': 40
+                },
+                {
+                  'label': '2003',
+                  'value': 20
+                }
+            ]
+
+        return chart_data
