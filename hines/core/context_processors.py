42d89f7ce4d5b3d7bc5cebcb97e242f639ba2016
Phil Gyford <phil@gyford.com>
2019-02-06 19:10:56 +0000

    Add author to bottom of blog posts

--- hines/core/context_processors.py
+++ hines/core/context_processors.py
@@ -1,5 +1,7 @@
 from django.contrib.sites.shortcuts import get_current_site
+
 from hines.core import app_settings
+from hines.core.utils import get_site_url
 
 
 # Things that we want to be available in the context of every page.
@@ -12,6 +14,7 @@ def core(request):
 
     return {
         'site_name': current_site.name,
+        'site_url': get_site_url(),
         'google_analytics_id': app_settings.GOOGLE_ANALYTICS_ID,
         'show_grid': show_grid,
         'author_name': app_settings.AUTHOR_NAME,
