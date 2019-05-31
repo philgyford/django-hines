7b666ed4f76ffd6165e19b1a461499a3bfc4f6c8
Phil Gyford <phil@gyford.com>
2019-05-31 15:58:21 +0100

    Formatting

--- tests/weblogs/models/test_post.py
+++ tests/weblogs/models/test_post.py
@@ -10,26 +10,29 @@ from tests import override_app_settings
 from hines.core.utils import make_datetime
 from hines.weblogs.models import Post
 from hines.weblogs.factories import (
-    BlogFactory, DraftPostFactory, LivePostFactory, ScheduledPostFactory,
-    TrackbackFactory
+    BlogFactory,
+    DraftPostFactory,
+    LivePostFactory,
+    ScheduledPostFactory,
+    TrackbackFactory,
 )
 
 
 class PostTestCase(TestCase):
-
     def test_str(self):
-        post = LivePostFactory(title='My Blog Post')
-        self.assertEqual(str(post), 'My Blog Post')
+        post = LivePostFactory(title="My Blog Post")
+        self.assertEqual(str(post), "My Blog Post")
 
     def test_ordering(self):
         publish_base = timezone.now()
         # Published later; should be third.
-        p3 = LivePostFactory(title='B',
-                time_published=publish_base - datetime.timedelta(hours=1))
+        p3 = LivePostFactory(
+            title="B", time_published=publish_base - datetime.timedelta(hours=1)
+        )
         # Most recently published; should be first:
-        p1 = LivePostFactory(title='C', time_published=publish_base)
+        p1 = LivePostFactory(title="C", time_published=publish_base)
         # Published the same time as 1 but created earlier; should be second:
-        p2 = LivePostFactory(title='A', time_published=publish_base)
+        p2 = LivePostFactory(title="A", time_published=publish_base)
 
         # Adjust their automatically-generated time_createds:
         created_base = timezone.now() - datetime.timedelta(days=1)
@@ -53,24 +56,33 @@ class PostTestCase(TestCase):
 
     def test_intro_html_convert_line_breaks(self):
         "It should add <p> and <br> tags when converting line breaks."
-        post = LivePostFactory(html_format=Post.CONVERT_LINE_BREAKS_FORMAT,
-                intro="""<a href="http://example.org">Hello</a>.
-Another line.""")
-        self.assertEqual(post.intro_html,
-            '<p><a href="http://example.org">Hello</a>.<br>Another line.</p>')
+        post = LivePostFactory(
+            html_format=Post.CONVERT_LINE_BREAKS_FORMAT,
+            intro="""<a href="http://example.org">Hello</a>.
+Another line.""",
+        )
+        self.assertEqual(
+            post.intro_html,
+            '<p><a href="http://example.org">Hello</a>.<br>Another line.</p>',
+        )
 
     def test_intro_html_markdown(self):
         "It should convert markdown to html."
-        post = LivePostFactory(html_format=Post.MARKDOWN_FORMAT,
-                intro="[Hello](http://example.org). *OK?*")
-        self.assertEqual(post.intro_html,
-            '<p><a href="http://example.org">Hello</a>. <em>OK?</em></p>')
+        post = LivePostFactory(
+            html_format=Post.MARKDOWN_FORMAT, intro="[Hello](http://example.org). *OK?*"
+        )
+        self.assertEqual(
+            post.intro_html,
+            '<p><a href="http://example.org">Hello</a>. <em>OK?</em></p>',
+        )
 
     def test_intro_html_smartypants(self):
-        post = LivePostFactory(html_format=Post.NO_FORMAT,
-                                intro="""This... isn't -- "special".""")
-        self.assertEqual(post.intro_html,
-                        "This&#8230; isn&#8217;t &#8212; &#8220;special&#8221;.")
+        post = LivePostFactory(
+            html_format=Post.NO_FORMAT, intro="""This... isn't -- "special"."""
+        )
+        self.assertEqual(
+            post.intro_html, "This&#8230; isn&#8217;t &#8212; &#8220;special&#8221;."
+        )
 
     def test_body_html_no_format(self):
         "With no formmating, body_html should be the same as body."
@@ -80,41 +92,58 @@ Another line.""")
 
     def test_body_html_convert_line_breaks(self):
         "It should add <p> and <br> tags when converting line breaks."
-        post = LivePostFactory(html_format=Post.CONVERT_LINE_BREAKS_FORMAT,
-                body="""<a href="http://example.org">Hello</a>.
-Another line.""")
-        self.assertEqual(post.body_html,
-            '<p><a href="http://example.org">Hello</a>.<br>Another line.</p>')
+        post = LivePostFactory(
+            html_format=Post.CONVERT_LINE_BREAKS_FORMAT,
+            body="""<a href="http://example.org">Hello</a>.
+Another line.""",
+        )
+        self.assertEqual(
+            post.body_html,
+            '<p><a href="http://example.org">Hello</a>.<br>Another line.</p>',
+        )
 
     def test_body_html_markdown(self):
         "It should convert markdown to html."
-        post = LivePostFactory(html_format=Post.MARKDOWN_FORMAT,
-                body="[Hello](http://example.org). *OK?*")
-        self.assertEqual(post.body_html,
-            '<p><a href="http://example.org">Hello</a>. <em>OK?</em></p>')
+        post = LivePostFactory(
+            html_format=Post.MARKDOWN_FORMAT, body="[Hello](http://example.org). *OK?*"
+        )
+        self.assertEqual(
+            post.body_html,
+            '<p><a href="http://example.org">Hello</a>. <em>OK?</em></p>',
+        )
 
     def test_body_html_smartypants(self):
-        post = LivePostFactory(html_format=Post.NO_FORMAT,
-                                body="""This... isn't -- "special".""")
-        self.assertEqual(post.body_html,
-                        "This&#8230; isn&#8217;t &#8212; &#8220;special&#8221;.")
+        post = LivePostFactory(
+            html_format=Post.NO_FORMAT, body="""This... isn't -- "special"."""
+        )
+        self.assertEqual(
+            post.body_html, "This&#8230; isn&#8217;t &#8212; &#8220;special&#8221;."
+        )
 
     def test_new_excerpt(self):
         "If excerpt is not set, it should be created on save."
-        post = LivePostFactory(html_format=Post.NO_FORMAT,
-                intro='<p><a href="http://example.org">Hello.</a></p>',
-                body='<p>The "body" goes on for a bit so we can check the excerpt is truncated and working correctly as we would really expect it to do.</p>',
-                excerpt='')
+        post = LivePostFactory(
+            html_format=Post.NO_FORMAT,
+            intro='<p><a href="http://example.org">Hello.</a></p>',
+            body='<p>The "body" goes on for a bit so we can check the '
+            'excerpt is truncated and working correctly as we would '
+            'really expect it to do.</p>',
+            excerpt="",
+        )
         # Note: curly quotes created by Smartypants, and decoded in
         # Post.make_excerpt():
-        self.assertEqual(post.excerpt,
-            'Hello. The “body” goes on for a bit so we can check the excerpt is truncated and working correctly…')
+        self.assertEqual(
+            post.excerpt,
+            "Hello. The “body” goes on for a bit so we can check the "
+            "excerpt is truncated and working correctly…",
+        )
 
     def test_existing_excerpt(self):
         "If the excerpt it set, it isnt overwritten on save."
-        post = LivePostFactory(intro='The intro', body='The body',
-                excerpt='The excerpt')
-        self.assertEqual(post.excerpt, 'The excerpt')
+        post = LivePostFactory(
+            intro="The intro", body="The body", excerpt="The excerpt"
+        )
+        self.assertEqual(post.excerpt, "The excerpt")
 
     @freeze_time("2017-07-01 12:00:00", tz_offset=-8)
     def test_time_published_is_set(self):
@@ -132,11 +161,11 @@ Another line.""")
         post = LivePostFactory(time_published=time_published)
 
         # Unpublish it:
-        post.status=Post.DRAFT_STATUS
+        post.status = Post.DRAFT_STATUS
         post.save()
 
         # Republish it:
-        post.status=Post.LIVE_STATUS
+        post.status = Post.LIVE_STATUS
         post.save()
 
         # Hasn't changed to now:
@@ -144,52 +173,62 @@ Another line.""")
 
     def test_default_manager(self):
         "It should include posts of all statuses."
-        live_post = LivePostFactory()
-        draft_post = DraftPostFactory()
-        scheduled_post = ScheduledPostFactory()
+        LivePostFactory()
+        DraftPostFactory()
+        ScheduledPostFactory()
         posts = Post.objects.all()
         self.assertEqual(len(posts), 3)
 
     def test_public_posts_manager(self):
         "It should only include published posts."
         live_post = LivePostFactory()
-        draft_post = DraftPostFactory()
-        scheduled_post = ScheduledPostFactory()
+        DraftPostFactory()
+        ScheduledPostFactory()
         posts = Post.public_objects.all()
         self.assertEqual(len(posts), 1)
         self.assertEqual(posts[0], live_post)
 
     def test_get_absolute_url(self):
-        blog = BlogFactory(slug='writing')
-        post = LivePostFactory(blog=blog, slug='my-post',
-                           time_published=make_datetime('2017-04-03 12:00:00'))
-        self.assertEqual(post.get_absolute_url(),
-                            '/terry/writing/2017/04/03/my-post/')
+        blog = BlogFactory(slug="writing")
+        post = LivePostFactory(
+            blog=blog,
+            slug="my-post",
+            time_published=make_datetime("2017-04-03 12:00:00"),
+        )
+        self.assertEqual(post.get_absolute_url(), "/terry/writing/2017/04/03/my-post/")
 
     def test_get_next_post(self):
         "It should not return draft posts or posts from other blogs."
         blog = BlogFactory()
-        post = LivePostFactory(blog=blog,
-                           time_published=make_datetime('2017-04-03 12:00:00'))
-        draft_post = DraftPostFactory(blog=blog,
-                           time_published=make_datetime('2017-04-04 12:00:00'))
-        other_blogs_post = LivePostFactory(
-                           time_published=make_datetime('2017-04-04 12:00:00'))
-        next_post = LivePostFactory(blog=blog,
-                           time_published=make_datetime('2017-04-05 12:00:00'))
+        post = LivePostFactory(
+            blog=blog, time_published=make_datetime("2017-04-03 12:00:00")
+        )
+        DraftPostFactory(
+            blog=blog, time_published=make_datetime("2017-04-04 12:00:00")
+        )
+        LivePostFactory(
+            time_published=make_datetime("2017-04-04 12:00:00")
+        )
+        next_post = LivePostFactory(
+            blog=blog, time_published=make_datetime("2017-04-05 12:00:00")
+        )
         self.assertEqual(post.get_next_post(), next_post)
 
     def test_get_previous_post(self):
         "It should not return draft posts or posts from other blogs."
         blog = BlogFactory()
-        post = LivePostFactory(blog=blog,
-                           time_published=make_datetime('2017-04-05 12:00:00'))
-        draft_post = DraftPostFactory(blog=blog,
-                           time_published=make_datetime('2017-04-04 12:00:00'))
-        other_blogs_post = LivePostFactory(
-                           time_published=make_datetime('2017-04-04 12:00:00'))
-        previous_post = LivePostFactory(blog=blog,
-                           time_published=make_datetime('2017-04-03 12:00:00'))
+        post = LivePostFactory(
+            blog=blog, time_published=make_datetime("2017-04-05 12:00:00")
+        )
+        DraftPostFactory(
+            blog=blog, time_published=make_datetime("2017-04-04 12:00:00")
+        )
+        LivePostFactory(
+            time_published=make_datetime("2017-04-04 12:00:00")
+        )
+        previous_post = LivePostFactory(
+            blog=blog, time_published=make_datetime("2017-04-03 12:00:00")
+        )
         self.assertEqual(post.get_previous_post(), previous_post)
 
     def test_tags(self):
@@ -197,21 +236,21 @@ Another line.""")
         # Don't want to test everything about django-taggit.
         # Just make sure it's generall working on Posts.
         post = LivePostFactory()
-        post.tags.add('Haddock', 'Sea', 'Fish')
+        post.tags.add("Haddock", "Sea", "Fish")
         tags = post.get_tags()
         self.assertEqual(len(tags), 3)
-        self.assertEqual(tags[0].name, 'Fish')
-        self.assertEqual(tags[0].slug, 'fish')
-        self.assertEqual(tags[1].name, 'Haddock')
-        self.assertEqual(tags[1].slug, 'haddock')
-        self.assertEqual(tags[2].name, 'Sea')
-        self.assertEqual(tags[2].slug, 'sea')
+        self.assertEqual(tags[0].name, "Fish")
+        self.assertEqual(tags[0].slug, "fish")
+        self.assertEqual(tags[1].name, "Haddock")
+        self.assertEqual(tags[1].slug, "haddock")
+        self.assertEqual(tags[2].name, "Sea")
+        self.assertEqual(tags[2].slug, "sea")
 
     def test_get_visible_trackbacks(self):
         "It should only return visible Trackbacks."
         post = LivePostFactory()
         visible_tb = TrackbackFactory(post=post, is_visible=True)
-        invisible_tb = TrackbackFactory(post=post, is_visible=False)
+        TrackbackFactory(post=post, is_visible=False)
 
         trackbacks = post.get_visible_trackbacks()
 
@@ -220,28 +259,35 @@ Another line.""")
 
     def test_status_str(self):
         post = LivePostFactory()
-        self.assertEqual(post.status_str, 'Published')
+        self.assertEqual(post.status_str, "Published")
 
     def test_main_image_url_none(self):
-        "If there's no image in either intro_html or body_html it should return an empty string."
-        post = LivePostFactory(html_format=Post.NO_FORMAT,
-                                intro="<p>Hello.</p>",
-                                body="<p>Bye.</p>")
-        self.assertEqual(post.main_image_url, '')
+        """If there's no image in either intro_html or body_html it
+        should return an empty string.
+        """
+        post = LivePostFactory(
+            html_format=Post.NO_FORMAT, intro="<p>Hello.</p>", body="<p>Bye.</p>"
+        )
+        self.assertEqual(post.main_image_url, "")
 
     def test_main_image_url_from_intro(self):
         "It should return the URL of an image from intro_html if there is one."
-        post = LivePostFactory(html_format=Post.NO_FORMAT,
-                intro='<p>Hello. <img src="/dir/img1.jpg">. Bye.</p>',
-                body='<p>Hello. <img src="/dir/img2.jpg">. Bye.</p>')
-        self.assertEqual(post.main_image_url, '/dir/img1.jpg')
+        post = LivePostFactory(
+            html_format=Post.NO_FORMAT,
+            intro='<p>Hello. <img src="/dir/img1.jpg">. Bye.</p>',
+            body='<p>Hello. <img src="/dir/img2.jpg">. Bye.</p>',
+        )
+        self.assertEqual(post.main_image_url, "/dir/img1.jpg")
 
     def test_main_image_url_from_body(self):
         "If there's no image in intro_html, it should return the first from body_html"
-        post = LivePostFactory(html_format=Post.NO_FORMAT,
-                intro='<p>Hello. Bye.</p>',
-                body='<p>Hello. <img src="/dir/img1.jpg"></p><p><img src="/dir/img2.jpg">. Bye.</p>')
-        self.assertEqual(post.main_image_url, '/dir/img1.jpg')
+        post = LivePostFactory(
+            html_format=Post.NO_FORMAT,
+            intro="<p>Hello. Bye.</p>",
+            body='<p>Hello. <img src="/dir/img1.jpg"></p><p>'
+            '<img src="/dir/img2.jpg">. Bye.</p>',
+        )
+        self.assertEqual(post.main_image_url, "/dir/img1.jpg")
 
     @override_app_settings(ALLOW_COMMENTS=False)
     def test_comments_allowed_settings(self):
@@ -274,7 +320,7 @@ Another line.""")
     @override_app_settings()
     def test_comments_allowed_no_setting(self):
         "If no setting, but blog and post are true, it should return True"
-        if hasattr(settings, 'HINES_ALLOW_COMMENTS'):
+        if hasattr(settings, "HINES_ALLOW_COMMENTS"):
             del settings.HINES_ALLOW_COMMENTS
         b = BlogFactory(allow_comments=True)
         p = LivePostFactory(blog=b, allow_comments=True)
