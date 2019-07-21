9be76e770fe17b6ab86d8fe11263fa4ecc046d90
Phil Gyford <phil@gyford.com>
2019-07-21 17:47:13 +0100

    Please the linter

--- hines/core/feeds.py
+++ hines/core/feeds.py
@@ -1,7 +1,7 @@
 from django.contrib.sites.models import Site
 from django.templatetags.static import static
 from django.contrib.syndication.views import Feed
-from django.template import Context, loader, TemplateDoesNotExist
+from django.template import loader, TemplateDoesNotExist
 from django.urls import reverse
 from django.utils.feedgenerator import Rss201rev2Feed
 from django.utils.html import escape
@@ -34,7 +34,7 @@ class ExtendedRSSFeed(Rss201rev2Feed):
 
     def root_attributes(self):
         attrs = super().root_attributes()
-        attrs['xmlns:content'] = 'http://purl.org/rss/1.0/modules/content/'
+        attrs["xmlns:content"] = "http://purl.org/rss/1.0/modules/content/"
         return attrs
 
     def add_root_elements(self, handler):
@@ -45,12 +45,12 @@ class ExtendedRSSFeed(Rss201rev2Feed):
         image_url = self.channel_image_url()
 
         if image_url:
-            handler.startElement('image', {})
-            handler.addQuickElement('url', image_url)
+            handler.startElement("image", {})
+            handler.addQuickElement("url", image_url)
             # Use the same title and link as the feed as a whole:
-            handler.addQuickElement('title', self.channel_image_title())
-            handler.addQuickElement('link', self.channel_image_link())
-            handler.endElement('image')
+            handler.addQuickElement("title", self.channel_image_title())
+            handler.addQuickElement("link", self.channel_image_link())
+            handler.endElement("image")
 
     def add_item_elements(self, handler, item):
         """
@@ -58,12 +58,12 @@ class ExtendedRSSFeed(Rss201rev2Feed):
         """
         super().add_item_elements(handler, item)
 
-        if item['content'] is not None:
-            handler.startElement('content:encoded', {})
+        if item["content"] is not None:
+            handler.startElement("content:encoded", {})
 
-            content = '<![CDATA['
-            content += item['content']
-            content += ']]>'
+            content = "<![CDATA["
+            content += item["content"]
+            content += "]]>"
 
             # Adding content in this way do not escape content so make it
             # suitable for Feedburner and other services. If we use
@@ -71,16 +71,16 @@ class ExtendedRSSFeed(Rss201rev2Feed):
             # not work perfectly with Feedburner and other services.
             handler._write(content)
 
-            handler.endElement('content:encoded')
+            handler.endElement("content:encoded")
 
     def channel_image_url(self):
         "URL of the image to use for the feed."
         url = static(app_settings.SITE_ICON)
-        return '{}{}'.format(get_site_url(), url)
+        return "{}{}".format(get_site_url(), url)
 
     def channel_image_title(self):
         "Might be used for image alt tag."
-        return 'Site icon'
+        return "Site icon"
 
     def channel_image_link(self):
         "Might be used to link the image to the site."
@@ -92,6 +92,7 @@ class ExtendedFeed(Feed):
     Required to add content:encoded elements to a feed.
     See the comments in ExtendedRSSFeed.
     """
+
     # Specify the path to a template to use that for the content:encoded data.
     content_template = None
 
@@ -102,9 +103,7 @@ class ExtendedFeed(Feed):
         """
         extra = super().item_extra_kwargs(item)
 
-        extra.update({
-            'content': self.get_item_content(item),
-        })
+        extra.update({"content": self.get_item_content(item)})
         return extra
 
     def get_item_content(self, item):
@@ -121,8 +120,7 @@ class ExtendedFeed(Feed):
                 pass
 
         if content_tmp:
-            context = {'obj': item,
-                       'site_url': get_site_url(),}
+            context = {"obj": item, "site_url": get_site_url()}
             content = content_tmp.render(context)
         else:
             content = self.item_content(item)
@@ -133,11 +131,11 @@ class ExtendedFeed(Feed):
         """
         Only used if self.content_template is None.
         """
-        return ''
+        return ""
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['foo'] = 'bar'
+        context["foo"] = "bar"
         return context
 
 
@@ -147,9 +145,9 @@ class EverythingFeedRSS(ExtendedFeed):
 
     feed_type = ExtendedRSSFeed
 
-    description_template = 'hines_core/feeds/everything_description.html'
+    description_template = "hines_core/feeds/everything_description.html"
 
-    content_template = 'hines_core/feeds/everything_content.html'
+    content_template = "hines_core/feeds/everything_content.html"
 
     # Getting details about the feed/site:
 
@@ -160,10 +158,10 @@ class EverythingFeedRSS(ExtendedFeed):
         return get_site_url()
 
     def title(self, obj):
-        return '%s: Everything' % obj.name
+        return "%s: Everything" % obj.name
 
     def description(self, obj):
-        return 'Things written, created, linked to or liked by Phil Gyford'
+        return "Things written, created, linked to or liked by Phil Gyford"
 
     def items(self, obj):
         kinds = app_settings.EVERYTHING_FEED_KINDS
@@ -172,65 +170,69 @@ class EverythingFeedRSS(ExtendedFeed):
     # Getting details for each post in the feed:
 
     def item_title(self, item):
-        if item['kind'] == 'blog_post':
-            title = item['object'].title
+        if item["kind"] == "blog_post":
+            title = item["object"].title
 
-        elif item['kind'] == 'pinboard_bookmark':
-            title = "[Link] {}".format(item['object'].title)
+        elif item["kind"] == "pinboard_bookmark":
+            title = "[Link] {}".format(item["object"].title)
 
-        elif item['kind'] == 'flickr_photos':
-            title = 'Photos from {}'.format(item['time'].strftime('%-d %B %Y'))
+        elif item["kind"] == "flickr_photos":
+            title = "Photos from {}".format(item["time"].strftime("%-d %B %Y"))
         else:
             title = item.title
 
         return title
 
     def item_link(self, item):
-        if item['kind'] == 'flickr_photos':
-            t = item['time']
-            return reverse('hines:day_archive', kwargs={
-                                                    'year': t.year,
-                                                    'month': t.strftime('%m'),
-                                                    'day': t.strftime('%d') })
-        elif item['kind'] == 'blog_post' and item['object'].remote_url:
+        if item["kind"] == "flickr_photos":
+            t = item["time"]
+            return reverse(
+                "hines:day_archive",
+                kwargs={
+                    "year": t.year,
+                    "month": t.strftime("%m"),
+                    "day": t.strftime("%d"),
+                },
+            )
+        elif item["kind"] == "blog_post" and item["object"].remote_url:
             # e.g. A comment on another site; link to it there.
-            return item['object'].remote_url
-        elif item['kind'] == 'pinboard_bookmark':
+            return item["object"].remote_url
+        elif item["kind"] == "pinboard_bookmark":
             # Link to the bookmark's URL itself, not to our permalink.
-            return item['object'].url
+            return item["object"].url
         else:
-            return item['object'].get_absolute_url()
+            return item["object"].get_absolute_url()
 
     def item_pubdate(self, item):
-        if item['kind'] == 'blog_post':
-            return item['object'].time_published
+        if item["kind"] == "blog_post":
+            return item["object"].time_published
 
-        elif item['kind'] == 'pinboard_bookmark':
-            return item['object'].post_time
+        elif item["kind"] == "pinboard_bookmark":
+            return item["object"].post_time
 
-        elif item['kind'] == 'flickr_photos':
-            return item['objects'][0].post_time
+        elif item["kind"] == "flickr_photos":
+            return item["objects"][0].post_time
 
     def item_updateddate(self, item):
-        if item['kind'] == 'blog_post':
-            return item['object'].time_modified
+        if item["kind"] == "blog_post":
+            return item["object"].time_modified
 
-        elif item['kind'] == 'pinboard_bookmark':
-            return item['object'].time_modified
+        elif item["kind"] == "pinboard_bookmark":
+            return item["object"].time_modified
 
-        elif item['kind'] == 'flickr_photos':
+        elif item["kind"] == "flickr_photos":
             dt = None
-            for photo in item['objects']:
+            for photo in item["objects"]:
                 if dt is None or photo.last_update_time > dt:
                     dt = photo.last_update_time
             return dt
 
     def item_author_name(self, item):
         "We could show authors for other kinds of things, but we don't currently."
-        if item['kind'] == 'blog_post':
-            return item['object'].author.display_name
+        if item["kind"] == "blog_post":
+            return item["object"].author.display_name
 
     def item_author_email(self, item):
-        if item['kind'] == 'blog_post':
-            if item['object'].blog.show_author_email_in_feed:
-                return item['object'].author.email
+        if item["kind"] == "blog_post":
+            if item["object"].blog.show_author_email_in_feed:
+                return item["object"].author.email
