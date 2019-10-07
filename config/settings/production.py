38267ee2ec2ec1f663754bbacd91e62a45700a50
Phil Gyford <phil@gyford.com>
2019-10-07 14:37:24 +0100

    Define AWS_DEFAULT_ACL for botos3boto3storage

--- config/settings/production.py
+++ config/settings/production.py
@@ -2,9 +2,7 @@ from .base import *
 
 DEBUG = False
 
-ADMINS = [
-    ('Phil Gyford', 'phil@gyford.com'),
-]
+ADMINS = [("Phil Gyford", "phil@gyford.com")]
 
 MANAGERS = ADMINS
 
@@ -15,33 +13,34 @@ PREPEND_WWW = True
 
 # Storing Media files on AWS.
 
-DEFAULT_FILE_STORAGE = 'hines.core.storages.CustomS3Boto3Storage'
+DEFAULT_FILE_STORAGE = "hines.core.storages.CustomS3Boto3Storage"
 
-AWS_ACCESS_KEY_ID = get_env_variable('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = get_env_variable('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = get_env_variable('AWS_STORAGE_BUCKET_NAME')
+AWS_ACCESS_KEY_ID = get_env_variable("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = get_env_variable("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = get_env_variable("AWS_STORAGE_BUCKET_NAME")
 
 AWS_QUERYSTRING_AUTH = False
 
-MEDIA_URL = 'https://{}.s3.amazonaws.com{}'.format(
-                                            AWS_STORAGE_BUCKET_NAME, MEDIA_URL)
+AWS_DEFAULT_ACL = "public-read"
+
+MEDIA_URL = "https://{}.s3.amazonaws.com{}".format(AWS_STORAGE_BUCKET_NAME, MEDIA_URL)
 
 CACHES = {
-    'default': {
-        'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': get_env_variable('REDIS_URL'),
-        'KEY_PREFIX': 'hines',
-        'OPTIONS': {
-            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": get_env_variable("REDIS_URL"),
+        "KEY_PREFIX": "hines",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
             # If Redis is down, ignore exceptions:
-            'IGNORE_EXCEPTIONS': True,
+            "IGNORE_EXCEPTIONS": True,
         },
     }
 }
 
 
 # https
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
@@ -58,15 +57,13 @@ CSRF_COOKIE_SECURE = True
 # https://devcenter.heroku.com/articles/sentry#integrating-with-python-or-django
 # via https://simonwillison.net/2017/Oct/17/free-continuous-deployment/# Step_4_Monitor_errors_with_Sentry_75
 
-SENTRY_DSN = os.environ.get('SENTRY_DSN')
+SENTRY_DSN = os.environ.get("SENTRY_DSN")
 
 if SENTRY_DSN:
-    INSTALLED_APPS += (
-        'raven.contrib.django.raven_compat',
-    )
+    INSTALLED_APPS += ("raven.contrib.django.raven_compat",)
     RAVEN_CONFIG = {
-        'dsn': SENTRY_DSN,
-        'release': os.environ.get('HEROKU_SLUG_COMMIT', ''),
+        "dsn": SENTRY_DSN,
+        "release": os.environ.get("HEROKU_SLUG_COMMIT", ""),
     }
 
 
