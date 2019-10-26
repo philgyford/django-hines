3ac4dddbaaf36fd1ae9f8d468ccb7b698273cb33
Phil Gyford <phil@gyford.com>
2019-10-26 13:54:27 +0100

    Formatting

--- tests/core/test_utils.py
+++ tests/core/test_utils.py
@@ -4,75 +4,114 @@ from hines.core.utils import markdownify, truncate_string
 
 
 class MarkdownifyTestCase(TestCase):
-
     def test_markdownify(self):
         "Just test it actually does some Markdowning"
         html = markdownify("Hello\n\nBye")
-        self.assertEqual(html,
-            "<p>Hello</p>\n<p>Bye</p>")
+        self.assertEqual(html, "<p>Hello</p>\n<p>Bye</p>")
 
     def test_markdownify_fenced_code(self):
         "It should mark up code blocks correctly."
         html = markdownify("```\nCode line 1\n\nCode line 2\n```")
-        self.assertEqual(html,
-            "<pre><code>Code line 1\n\nCode line 2\n</code></pre>")
+        self.assertEqual(html, "<pre><code>Code line 1\n\nCode line 2\n</code></pre>")
+
 
 class TruncateStringTestCase(TestCase):
-
     def test_truncate_string_strip_html(self):
         "By default, strips HTML"
         self.assertEqual(
-            truncate_string('<p>Some text. <a href="http://www.example.com/"><b>A link</b></a>. And more.'),
-            u'Some text. A link. And more.'
+            truncate_string(
+                '<p>Some text. '
+                '<a href="http://www.example.com/"><b>A link</b></a>. And more.'
+            ),
+            "Some text. A link. And more.",
         )
 
     def test_truncate_string_strip_html_false(self):
         "Can be told not to strip HTML"
         self.assertEqual(
-            truncate_string('<p>Some text. <a href="http://www.example.com/"><b>A link</b></a>. And more.', strip_html=False),
-            u'<p>Some text. <a href="http://www.example.com/"><b>A link</b></a>. And more.'
+            truncate_string(
+                '<p>Some text. '
+                '<a href="http://www.example.com/"><b>A link</b></a>. And more.',
+                strip_html=False,
+            ),
+            '<p>Some text. '
+            '<a href="http://www.example.com/"><b>A link</b></a>. And more.',
         )
 
     def test_truncate_string_default_chars(self):
         "By default, trims to 255 characters"
         self.assertEqual(
-            truncate_string('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget odio eget odio porttitor accumsan in eget elit. Integer gravida egestas nunc. Mauris at tortor ornare, blandit eros quis, auctor lacus. Fusce ullamcorper nunc vitae tincidunt sodales. Vestibulum sit amet lacus at sem porta porta.'),
-            u'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget odio eget odio porttitor accumsan in eget elit. Integer gravida egestas nunc. Mauris at tortor ornare, blandit eros quis, auctor lacus. Fusce ullamcorper nunc vitae tincidunt sodales. Ve…'
+            truncate_string(
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget "
+                "odio eget odio porttitor accumsan in eget elit. Integer gravida "
+                "egestas nunc. Mauris at tortor ornare, blandit eros quis, auctor "
+                "lacus. Fusce ullamcorper nunc vitae tincidunt sodales. Vestibulum sit "
+                "amet lacus at sem porta porta."
+            ),
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget "
+            "odio eget odio porttitor accumsan in eget elit. Integer gravida "
+            "egestas nunc. Mauris at tortor ornare, blandit eros quis, auctor "
+            "lacus. Fusce ullamcorper nunc vitae tincidunt sodales. Ve…",
         )
 
     def test_truncate_string_custom_chars(self):
         "Can be told to truncate to other lengths"
         self.assertEqual(
-            truncate_string('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget odio eget odio porttitor accumsan in eget elit. Integer gravida egestas nunc. Mauris at tortor ornare, blandit eros quis, auctor lacus. Fusce ullamcorper nunc vitae tincidunt sodales. Vestibulum sit amet lacus at sem porta porta.', chars=100),
-            u'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget odio eget odio porttitor accums…'
+            truncate_string(
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget "
+                "odio eget odio porttitor accumsan in eget elit. Integer gravida "
+                "egestas nunc. Mauris at tortor ornare, blandit eros quis, auctor "
+                "lacus. Fusce ullamcorper nunc vitae tincidunt sodales. Vestibulum sit "
+                "amet lacus at sem porta porta.",
+                chars=100,
+            ),
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget "
+            "odio eget odio porttitor accums…",
         )
 
     def test_truncate_string_truncate(self):
         "Can be given a custom 'truncate' string"
         self.assertEqual(
-            truncate_string('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget odio eget odio porttitor accumsan in eget elit. Integer gravida egestas nunc. Mauris at tortor ornare, blandit eros quis, auctor lacus. Fusce ullamcorper nunc vitae tincidunt sodales. Vestibulum sit amet lacus at sem porta porta.', truncate=' (cont.)'),
-            u'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget odio eget odio porttitor accumsan in eget elit. Integer gravida egestas nunc. Mauris at tortor ornare, blandit eros quis, auctor lacus. Fusce ullamcorper nunc vitae tincidunt soda (cont.)'
+            truncate_string(
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget "
+                "odio eget odio porttitor accumsan in eget elit. Integer gravida "
+                "egestas nunc. Mauris at tortor ornare, blandit eros quis, auctor "
+                "lacus. Fusce ullamcorper nunc vitae tincidunt sodales. Vestibulum sit "
+                "amet lacus at sem porta porta.",
+                truncate=" (cont.)",
+            ),
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget "
+            "odio eget odio porttitor accumsan in eget elit. Integer gravida "
+            "egestas nunc. Mauris at tortor ornare, blandit eros quis, auctor "
+            "lacus. Fusce ullamcorper nunc vitae tincidunt soda (cont.)",
         )
 
     def test_at_word_boundary(self):
         "Will break at word boundaries."
         self.assertEqual(
-            truncate_string(u'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget odio eget odio porttitor accumsan in eget elit. Integer gravida egestas nunc. Mauris at tortor ornare, blandit eros quis, auctor lacus. Fusce ullamcorper nunc vitae tincidunt sodales. Vestibulum sit amet lacus at sem porta porta.', at_word_boundary=True),
-            u'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget odio eget odio porttitor accumsan in eget elit. Integer gravida egestas nunc. Mauris at tortor ornare, blandit eros quis, auctor lacus. Fusce ullamcorper nunc vitae tincidunt sodales.…'
+            truncate_string(
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget "
+                "odio eget odio porttitor accumsan in eget elit. Integer gravida "
+                "egestas nunc. Mauris at tortor ornare, blandit eros quis, auctor "
+                "lacus. Fusce ullamcorper nunc vitae tincidunt sodales. Vestibulum sit "
+                "amet lacus at sem porta porta.",
+                at_word_boundary=True,
+            ),
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget "
+            "odio eget odio porttitor accumsan in eget elit. Integer gravida "
+            "egestas nunc. Mauris at tortor ornare, blandit eros quis, auctor "
+            "lacus. Fusce ullamcorper nunc vitae tincidunt sodales.…",
         )
 
     def test_no_truncation(self):
         """Too short to be truncated."""
-        self.assertEqual(
-            truncate_string(u'This is my string.'),
-            u'This is my string.'
-        )
+        self.assertEqual(truncate_string(u"This is my string."), "This is my string.")
 
     def test_no_truncation_at_word_boundary(self):
         """Too short to be truncated."""
         self.assertEqual(
-            truncate_string(u'This is my string.', at_word_boundary=True),
-            u'This is my string.'
+            truncate_string(u"This is my string.", at_word_boundary=True),
+            "This is my string.",
         )
 
     def test_truncate_string_all(self):
@@ -80,8 +119,16 @@ class TruncateStringTestCase(TestCase):
         and add custom string.
         """
         self.assertEqual(
-            truncate_string("""<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget odio eget odio porttitor accumsan in eget elit. Integer gravida egestas nunc. Mauris at tortor ornare, blandit eros quis, auctorlacus.</p>
+            truncate_string(
+                """<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget odio eget odio porttitor accumsan in eget elit. Integer gravida egestas nunc. Mauris at tortor ornare, blandit eros quis, auctorlacus.</p>
 
-        <p>Fusce ullamcorper nunc vitae tincidunt sodales. Vestibulum sit amet lacus at sem porta porta. Donec fringilla laoreet orci eu porta. Aenean non lacus hendrerit, semper odio a, feugiat orci. Suspendisse potenti.</p>""", strip_html=True, chars=200, truncate='...', at_word_boundary=True),
-            u'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget odio eget odio porttitor accumsan in eget elit. Integer gravida egestas nunc. Mauris at tortor ornare, blandit eros quis,...'
+        <p>Fusce ullamcorper nunc vitae tincidunt sodales. Vestibulum sit amet lacus at sem porta porta. Donec fringilla laoreet orci eu porta. Aenean non lacus hendrerit, semper odio a, feugiat orci. Suspendisse potenti.</p>""",  # noqa: E501
+                strip_html=True,
+                chars=200,
+                truncate="...",
+                at_word_boundary=True,
+            ),
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget "
+            "odio eget odio porttitor accumsan in eget elit. Integer gravida "
+            "egestas nunc. Mauris at tortor ornare, blandit eros quis,...",
         )
