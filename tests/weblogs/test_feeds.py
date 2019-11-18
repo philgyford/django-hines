c7d7ef12b5b2390be642df749c4c1203191f1c3c
Phil Gyford <phil@gyford.com>
2019-11-18 18:29:27 +0000

    Formatting

--- tests/weblogs/test_feeds.py
+++ tests/weblogs/test_feeds.py
@@ -3,57 +3,56 @@ from django.utils.feedgenerator import rfc2822_date
 
 from hines.core.utils import make_datetime
 from hines.users.factories import UserFactory
-from hines.weblogs.factories import BlogFactory, DraftPostFactory,\
-        LivePostFactory
+from hines.weblogs.factories import BlogFactory, DraftPostFactory, LivePostFactory
 from tests.core.test_feeds import FeedTestCase
 
 
 class BlogPostsFeedRSSTestCase(FeedTestCase):
-
     def setUp(self):
         super().setUp()
         self.user = UserFactory(
-                    first_name='Bob',
-                    last_name='Ferris',
-                    email='bob@example.org')
+            first_name="Bob", last_name="Ferris", email="bob@example.org"
+        )
         self.blog = BlogFactory(
-                        name='My Blog',
-                        slug='my-blog',
-                        feed_title='My Feed Title',
-                        feed_description='My feed description.',
-                        show_author_email_in_feed=True)
+            name="My Blog",
+            slug="my-blog",
+            feed_title="My Feed Title",
+            feed_description="My feed description.",
+            show_author_email_in_feed=True,
+        )
         # 5 older LIVE posts and then 1 new post that we test in more detail:
-        posts = LivePostFactory.create_batch(5,
-                            blog=self.blog,
-                            time_published=make_datetime('2017-04-22 15:00:00'),
-                            tags=['Dogs']
-                        )
+        LivePostFactory.create_batch(
+            5,
+            blog=self.blog,
+            time_published=make_datetime("2017-04-22 15:00:00"),
+            tags=["Dogs"],
+        )
         self.post2 = LivePostFactory(
-                            title='Bob\'s "latest" <cite>Cited</cite> <strong>post</strong>',
-                            slug='my-latest-post',
-                            excerpt='This is my <b>excerpt</b>.',
-                            intro="The post intro.",
-                            body="This is the post <b>body</b>.\n\nOK?",
-                            author=self.user,
-                            blog=self.blog,
-                            time_published=make_datetime('2017-04-25 16:00:00'),
-                            tags=['Fish', 'Dogs', 'Cats',]
-                        )
+            title='Bob\'s "latest" <cite>Cited</cite> <strong>post</strong>',
+            slug="my-latest-post",
+            excerpt="This is my <b>excerpt</b>.",
+            intro="The post intro.",
+            body="This is the post <b>body</b>.\n\nOK?",
+            author=self.user,
+            blog=self.blog,
+            time_published=make_datetime("2017-04-25 16:00:00"),
+            tags=["Fish", "Dogs", "Cats"],
+        )
         # These shouldn't appear in our feed:
         self.draft_post = DraftPostFactory(blog=self.blog)
         self.other_blogs_post = LivePostFactory()
 
         # To ensure our requests for the feed always use a specific domain:
         site = Site.objects.get()
-        site.domain = 'example.com'
+        site.domain = "example.com"
         site.save()
 
     def test_response_200(self):
-        response = self.client.get('/terry/my-blog/feeds/posts/rss/')
+        response = self.client.get("/terry/my-blog/feeds/posts/rss/")
         self.assertEqual(response.status_code, 200)
 
     def test_response_404(self):
-        response = self.client.get('/terry/not-my-blog/feeds/posts/rss/')
+        response = self.client.get("/terry/not-my-blog/feeds/posts/rss/")
         self.assertEqual(response.status_code, 404)
 
     def test_feed(self):
@@ -61,87 +60,117 @@ class BlogPostsFeedRSSTestCase(FeedTestCase):
         Borrowing a lot from
         https://github.com/django/django/blob/master/tests/syndication_tests/tests.py
         """
-        channel = self.get_feed_channel('/terry/my-blog/feeds/posts/rss/')
+        channel = self.get_feed_channel("/terry/my-blog/feeds/posts/rss/")
 
-        d = self.blog.public_posts.latest('time_modified').time_modified
+        d = self.blog.public_posts.latest("time_modified").time_modified
         last_build_date = rfc2822_date(d)
 
         # TEST THE channel ELEMENT
 
         # We're not currently using 'ttl', 'copyright' or 'category':
         self.assertChildNodes(
-            channel, [
-                'title', 'link', 'description', 'language', 'lastBuildDate',
-                'item', 'image', 'atom:link',
-            ]
+            channel,
+            [
+                "title",
+                "link",
+                "description",
+                "language",
+                "lastBuildDate",
+                "item",
+                "image",
+                "atom:link",
+            ],
         )
 
-        self.assertEqual(channel.attributes['xmlns:content'].value,
-                        'http://purl.org/rss/1.0/modules/content/')
+        self.assertEqual(
+            channel.attributes["xmlns:content"].value,
+            "http://purl.org/rss/1.0/modules/content/",
+        )
 
-        self.assertChildNodeContent(channel, {
-            'title': 'My Feed Title',
-            'description': 'My feed description.',
-            'link': 'http://example.com/terry/my-blog/',
-            'language': 'en',
-            'lastBuildDate': last_build_date,
-        })
+        self.assertChildNodeContent(
+            channel,
+            {
+                "title": "My Feed Title",
+                "description": "My feed description.",
+                "link": "http://example.com/terry/my-blog/",
+                "language": "en",
+                "lastBuildDate": last_build_date,
+            },
+        )
 
         # TEST THE channel image ELEMENT
 
-        image_el = channel.getElementsByTagName('image')[0]
+        image_el = channel.getElementsByTagName("image")[0]
 
-        self.assertChildNodes(image_el, ['url', 'title', 'link',])
+        self.assertChildNodes(image_el, ["url", "title", "link"])
 
-        self.assertChildNodeContent(image_el, {
-            # This gets a hash on the end from WhiteNoise, so leaving it off
-            # the test for now:
-            # 'url': 'http://example.com/static/img/site_icon.jpg',
-            'title': 'Site icon',
-            'link': 'http://example.com'
-        })
+        self.assertChildNodeContent(
+            image_el,
+            {
+                # This gets a hash on the end from WhiteNoise, so leaving it off
+                # the test for now:
+                # 'url': 'http://example.com/static/img/site_icon.jpg',
+                "title": "Site icon",
+                "link": "http://example.com",
+            },
+        )
 
         # TEST THE item ELEMENTS
 
-        items = channel.getElementsByTagName('item')
+        items = channel.getElementsByTagName("item")
         self.assertEqual(len(items), 5)
 
         # Test the content of the most recent Post:
 
-        self.assertChildNodeContent(items[0], {
-            'title': 'Bob\'s "latest" ‘Cited’ post',
-            'description': 'This is my excerpt.',
-            'link': 'http://example.com/terry/my-blog/2017/04/25/my-latest-post/',
-            'guid': 'http://example.com/terry/my-blog/2017/04/25/my-latest-post/',
-            'pubDate': rfc2822_date(self.post2.time_published),
-            'author': 'bob@example.org (Bob Ferris)',
-            'content:encoded': '<p>The post intro.</p><p>This is the post <b>body</b>.</p>\n<p>OK?</p>'
-        })
+        self.assertChildNodeContent(
+            items[0],
+            {
+                "title": 'Bob\'s "latest" ‘Cited’ post',
+                "description": "This is my excerpt.",
+                "link": "http://example.com/terry/my-blog/2017/04/25/my-latest-post/",
+                "guid": "http://example.com/terry/my-blog/2017/04/25/my-latest-post/",
+                "pubDate": rfc2822_date(self.post2.time_published),
+                "author": "bob@example.org (Bob Ferris)",
+                "content:encoded": "<p>The post intro.</p><p>This is the post <b>body</b>.</p>\n<p>OK?</p>",  # noqa: E501
+            },
+        )
 
-        self.assertCategories(items[0], ['Fish', 'Dogs', 'Cats'])
+        self.assertCategories(items[0], ["Fish", "Dogs", "Cats"])
 
         for item in items:
-            self.assertChildNodes(item,
-                    ['title', 'link', 'description', 'guid', 'category',
-                        'pubDate', 'author', 'content:encoded',])
+            self.assertChildNodes(
+                item,
+                [
+                    "title",
+                    "link",
+                    "description",
+                    "guid",
+                    "category",
+                    "pubDate",
+                    "author",
+                    "content:encoded",
+                ],
+            )
             # Assert that <guid> does not have any 'isPermaLink' attribute
-            self.assertIsNone(item.getElementsByTagName(
-                'guid')[0].attributes.get('isPermaLink'))
+            self.assertIsNone(
+                item.getElementsByTagName("guid")[0].attributes.get("isPermaLink")
+            )
 
     def test_no_author_email(self):
         "If we don't want to show author emails, they don't appear."
         self.blog.show_author_email_in_feed = False
         self.blog.save()
 
-        channel = self.get_feed_channel('/terry/my-blog/feeds/posts/rss/')
+        channel = self.get_feed_channel("/terry/my-blog/feeds/posts/rss/")
 
-        items = channel.getElementsByTagName('item')
+        items = channel.getElementsByTagName("item")
         self.assertEqual(len(items), 5)
 
         # It should have a <dc:creator> instead of <author>:
 
-        self.assertEqual(0, len( items[0].getElementsByTagName('author') ) )
+        self.assertEqual(0, len(items[0].getElementsByTagName("author")))
 
         self.assertEqual(
-            items[0].getElementsByTagName('dc:creator')[0].firstChild.wholeText,
-            'Bob Ferris')
+            items[0].getElementsByTagName("dc:creator")[0].firstChild.wholeText,
+            "Bob Ferris",
+        )
