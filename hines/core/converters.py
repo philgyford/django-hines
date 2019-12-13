2ee96478509691670b9a7cb266b6a359a98e519d
Phil Gyford <phil@gyford.com>
2019-12-13 16:57:24 +0000

    Formatting flake8/black

--- hines/core/converters.py
+++ hines/core/converters.py
@@ -3,44 +3,43 @@
 
 class FourDigitYearConverter:
     "Matches 4 digits."
-    regex = '[0-9]{4}'
+    regex = r"[0-9]{4}"
 
     def to_python(self, value):
         return int(value)
 
     def to_url(self, value):
-        return '{:04d}'.format(int(value))
+        return "{:04d}".format(int(value))
 
 
 class TwoDigitMonthConverter:
     "Matches 2 digits."
-    regex = '[0-9]{2}'
+    regex = r"[0-9]{2}"
 
     def to_python(self, value):
         return int(value)
 
     def to_url(self, value):
-        return '{:02d}'.format(int(value))
+        return "{:02d}".format(int(value))
 
 
 class TwoDigitDayConverter:
     "Matches 2 digits."
-    regex = '[0-9]{2}'
+    regex = r"[0-9]{2}"
 
     def to_python(self, value):
         return int(value)
 
     def to_url(self, value):
-        return '{:02d}'.format(int(value))
+        return "{:02d}".format(int(value))
 
 
 class WordCharacterConverter:
     "Matches a string of any 'word' characters; no punctuation."
-    regex = '\w+'
+    regex = r"\w+"
 
     def to_python(self, value):
         return str(value)
 
     def to_url(self, value):
         return value
-
