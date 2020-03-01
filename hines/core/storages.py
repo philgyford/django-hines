502ff2bd0d5feba14cc46d66951ff8ec738f3c59
Phil Gyford <phil@gyford.com>
2020-03-01 19:36:20 +0000

    Maybe fix error when uploading images to S3
    For #222

--- hines/core/storages.py
+++ hines/core/storages.py
@@ -7,13 +7,13 @@ class CustomS3Boto3Storage(S3Boto3Storage):
     """
     This is our custom version of S3Boto3Storage that fixes a bug in
     boto3 where the passed in file is closed upon upload.
-    From https://github.com/matthewwithanm/django-imagekit/issues/391#issuecomment-275367006
-
+    From:
+    https://github.com/matthewwithanm/django-imagekit/issues/391#issuecomment-275367006
     https://github.com/boto/boto3/issues/929
-    https://github.com/matthewwithanm/django-imagekit/issues/391
+    https://github.com/matthewwithanm/django-imagekit/issues/391#issuecomment-589850543
     """
 
-    def _save_content(self, obj, content, parameters):
+    def _save(self, name, content):
         """
         We create a clone of the content file as when this is passed to
         boto3 it wrongly closes the file upon upload where as the storage
@@ -22,17 +22,14 @@ class CustomS3Boto3Storage(S3Boto3Storage):
         # Seek our content back to the start
         content.seek(0, os.SEEK_SET)
 
-        # Create a temporary file that will write to disk after a specified size
-        content_autoclose = SpooledTemporaryFile()
+        # Create a temporary file that will write to disk after a specified
+        # size. This file will be automatically deleted when closed by
+        # boto3 or after exiting the `with` statement if the boto3 is fixed
+        with SpooledTemporaryFile() as content_autoclose:
 
-        # Write our original content into our copy that will be closed by boto3
-        content_autoclose.write(content.read())
+            # Write our original content into our copy that will be closed by boto3
+            content_autoclose.write(content.read())
 
-        # Upload the object which will auto close the content_autoclose instance
-        super(CustomS3Boto3Storage, self)._save_content(
-            obj, content_autoclose, parameters
-        )
-
-        # Cleanup if this is fixed upstream our duplicate should always close
-        if not content_autoclose.closed:
-            content_autoclose.close()
+            # Upload the object which will auto close the
+            # content_autoclose instance
+            super()._save(name, content_autoclose)
