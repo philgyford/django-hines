441c075ff221b6c1fecbe4eaefb06bb803fd387d
Phil Gyford <phil@gyford.com>
2020-05-17 16:14:56 +0100

    Move coverage config into setup.cfg
    For #226

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -6,6 +6,7 @@ set -e
 # ./run_tests.sh tests.appname.test_models.TestClass.test_a_thing
 TESTS_TO_RUN=${1:tests}
 
-coverage run --branch --source=. --omit=*/migrations/*.py,manage.py,tests/*.py manage.py test --settings=config.settings.tests $TESTS_TO_RUN
+coverage run manage.py test --settings=config.settings.tests $TESTS_TO_RUN
+flake8
 coverage report
 coverage html
