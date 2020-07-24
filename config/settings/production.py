255b6ea9746df3834927f195f9747a4d43d32f8b
Phil Gyford <phil@gyford.com>
2020-07-24 14:12:11 +0100

    Ready prodution settings for ADMIN comments feed settings For #99

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,4 +1,5 @@
 from .base import *  # noqa: F403
+from .base import get_env_variable
 import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
 
@@ -13,14 +14,22 @@ PREPEND_WWW = True
 # Close comments on posts after this many days (assuming they're open):
 HINES_COMMENTS_CLOSE_AFTER_DAYS = 30
 
+HINES_COMMENTS_ADMIN_NOT_PUBLISHED_FEED_SLUG = get_env_variable(
+    "HINES_COMMENTS_ADMIN_NOT_PUBLISHED_FEED_SLUG"
+)
+
+HINES_COMMENTS_ADMIN_PUBLISHED_FEED_SLUG = get_env_variable(
+    "HINES_COMMENTS_ADMIN_PUBLISHED_FEED_SLUG"
+)
+
 
 # Storing Media files on AWS.
 
 DEFAULT_FILE_STORAGE = "hines.core.storages.CustomS3Boto3Storage"
 
-AWS_ACCESS_KEY_ID = get_env_variable("AWS_ACCESS_KEY_ID")  # noqa: F405
-AWS_SECRET_ACCESS_KEY = get_env_variable("AWS_SECRET_ACCESS_KEY")  # noqa: F405
-AWS_STORAGE_BUCKET_NAME = get_env_variable("AWS_STORAGE_BUCKET_NAME")  # noqa: F405
+AWS_ACCESS_KEY_ID = get_env_variable("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = get_env_variable("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = get_env_variable("AWS_STORAGE_BUCKET_NAME")
 
 AWS_QUERYSTRING_AUTH = False
 
@@ -33,7 +42,7 @@ MEDIA_URL = "https://{}.s3.amazonaws.com{}".format(
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": get_env_variable("REDIS_URL"),  # noqa: F405
+        "LOCATION": get_env_variable("REDIS_URL"),
         "KEY_PREFIX": "hines",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
@@ -61,13 +70,13 @@ CSRF_COOKIE_SECURE = True
 # Sentry
 # https://devcenter.heroku.com/articles/sentry#integrating-with-python-or-django
 
-SENTRY_DSN = os.environ.get("SENTRY_DSN")  # noqa: F405
+SENTRY_DSN = get_env_variable("SENTRY_DSN")
 
 if SENTRY_DSN:
     sentry_sdk.init(
-        dsn=os.environ["SENTRY_DSN"],  # noqa: F40
+        dsn=get_env_variable("SENTRY_DSN"),
         integrations=[DjangoIntegration()],
-        release=os.environ.get("HEROKU_SLUG_COMMIT", ""),  # noqa: F40
+        release=get_env_variable("HEROKU_SLUG_COMMIT", ""),
     )
 
 #############################################################################
