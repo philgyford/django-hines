96d40ee31d9b068c6fc13123299a8e9b744fd6dd
Phil Gyford <phil@gyford.com>
2020-12-31 12:08:58 +0000

    Fix default variable assignment in shell scripts

--- scripts/pipenv-run-tests.sh
+++ scripts/pipenv-run-tests.sh
@@ -7,7 +7,7 @@ set -e
 # You can optionally pass in a test, or test module or class, as an argument.
 # e.g.
 # ./pipenv-run-tests.sh tests.appname.test_models.TestClass.test_a_thing
-TESTS_TO_RUN=${1:tests}
+TESTS_TO_RUN=${1:-tests}
 
 coverage run --branch --source=. --omit=*/migrations/*.py,manage.py,tests/*.py manage.py test --settings=config.settings.tests $TESTS_TO_RUN
 flake8
--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -8,6 +8,6 @@ set -e
 # You can optionally pass in a test, or test module or class, as an argument.
 # e.g.
 # ./run-tests.sh tests.appname.test_models.TestClass.test_a_thing
-TESTS_TO_RUN=${1:tests}
+TESTS_TO_RUN=${1:-tests}
 
 docker exec hines_web /bin/sh -c "pipenv run tests $TESTS_TO_RUN"
