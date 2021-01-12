22e9cda66ccef354a83193ff1f9eb373691d7472
Phil Gyford <phil@gyford.com>
2021-01-12 17:14:35 +0000

    Try tweaking CustomCommentAdminTestCase to avoid 'relation django_site does not exist'

--- tests/custom_comments/test_admin.py
+++ tests/custom_comments/test_admin.py
@@ -3,6 +3,7 @@ from django.test import TestCase
 
 from hines.custom_comments.factories import CommentFlagFactory, CustomCommentFactory
 from hines.custom_comments.admin import CustomCommentAdmin
+from hines.custom_comments.models import CustomComment
 from hines.weblogs.factories import LivePostFactory
 
 
@@ -10,7 +11,7 @@ class CustomCommentAdminTestCase(TestCase):
     def test_post_title(self):
         p = LivePostFactory(title="My post title")
         c = CustomCommentFactory(post=p)
-        cca = CustomCommentAdmin(c, AdminSite())
+        cca = CustomCommentAdmin(CustomComment, AdminSite())
         self.assertEqual(cca.post_title(c), "My post title")
 
     def test_flag(self):
@@ -18,11 +19,11 @@ class CustomCommentAdminTestCase(TestCase):
         c = CustomCommentFactory()
         CommentFlagFactory(comment=c, flag="This is spam")
         CommentFlagFactory(comment=c, flag="Hello")
-        cca = CustomCommentAdmin(c, AdminSite())
+        cca = CustomCommentAdmin(CustomComment, AdminSite())
         self.assertEqual(cca.flag(c), "Hello")
 
     def test_flag_none(self):
         "It should return an empty string if there are no flags."
         c = CustomCommentFactory()
-        cca = CustomCommentAdmin(c, AdminSite())
+        cca = CustomCommentAdmin(CustomComment, AdminSite())
         self.assertEqual(cca.flag(c), "")
