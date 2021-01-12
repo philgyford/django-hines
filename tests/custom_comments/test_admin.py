bbda25dd5b558105129ddfd98bd9bb282a80f04b
Phil Gyford <phil@gyford.com>
2021-01-12 16:53:24 +0000

    Try to fix 'relation django_site does not exist' when running tests

--- tests/custom_comments/test_admin.py
+++ tests/custom_comments/test_admin.py
@@ -7,13 +7,10 @@ from hines.weblogs.factories import LivePostFactory
 
 
 class CustomCommentAdminTestCase(TestCase):
-    def setUp(self):
-        self.site = AdminSite()
-
     def test_post_title(self):
         p = LivePostFactory(title="My post title")
         c = CustomCommentFactory(post=p)
-        cca = CustomCommentAdmin(c, self.site)
+        cca = CustomCommentAdmin(c, AdminSite())
         self.assertEqual(cca.post_title(c), "My post title")
 
     def test_flag(self):
@@ -21,11 +18,11 @@ class CustomCommentAdminTestCase(TestCase):
         c = CustomCommentFactory()
         CommentFlagFactory(comment=c, flag="This is spam")
         CommentFlagFactory(comment=c, flag="Hello")
-        cca = CustomCommentAdmin(c, self.site)
+        cca = CustomCommentAdmin(c, AdminSite())
         self.assertEqual(cca.flag(c), "Hello")
 
     def test_flag_none(self):
         "It should return an empty string if there are no flags."
         c = CustomCommentFactory()
-        cca = CustomCommentAdmin(c, self.site)
+        cca = CustomCommentAdmin(c, AdminSite())
         self.assertEqual(cca.flag(c), "")
