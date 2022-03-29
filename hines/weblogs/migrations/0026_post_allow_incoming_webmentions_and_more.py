9aa9b5aa1b26e88f69d7d6cc85dd9b1161140e5c
Phil Gyford <phil@gyford.com>
2022-03-29 16:58:41 +0100

    Formatting

--- hines/weblogs/migrations/0026_post_allow_incoming_webmentions_and_more.py
+++ hines/weblogs/migrations/0026_post_allow_incoming_webmentions_and_more.py
@@ -6,18 +6,18 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('weblogs', '0025_alter_taggedpost_tag'),
+        ("weblogs", "0025_alter_taggedpost_tag"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='post',
-            name='allow_incoming_webmentions',
+            model_name="post",
+            name="allow_incoming_webmentions",
             field=models.BooleanField(default=True),
         ),
         migrations.AddField(
-            model_name='post',
-            name='allow_outgoing_webmentions',
+            model_name="post",
+            name="allow_outgoing_webmentions",
             field=models.BooleanField(default=False),
         ),
     ]
