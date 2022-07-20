45f6775a8042057bb26732bf38bcb37d5ba9a46a
Phil Gyford <phil@gyford.com>
2022-07-20 14:57:32 +0100

    Add Rich for nicer logging, and add LOGGING settings

--- config/settings/base.py
+++ config/settings/base.py
@@ -213,6 +213,42 @@ SECURE_REFERRER_POLICY = "no-referrer-when-downgrade"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "filters": {
+        "require_debug_false": {
+            "()": "django.utils.log.RequireDebugFalse",
+        },
+        "require_debug_true": {
+            "()": "django.utils.log.RequireDebugTrue",
+        },
+    },
+    "formatters": {
+        "rich": {"datefmt": "[%X]"},
+    },
+    "handlers": {
+        "console": {
+            "class": "rich.logging.RichHandler",
+            "filters": ["require_debug_true"],
+            "formatter": "rich",
+            "level": "DEBUG",
+            "rich_tracebacks": True,
+            "tracebacks_show_locals": True,
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": [],
+            "level": "INFO",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "INFO",
+    },
+}
+
 ####################################################################
 # THIRD-PARTY APPS
 
